4dda3965853cd78582db17defb2d36c181e30fcf
SaidMayan <Kasemayan@gmail.com>
2018-06-13 19:47:42 -0400

    update project 1, 2 with images

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import { Jumbotron, Grid, Row, Col, Image, Button} from 'react-bootstrap';
 import './Home.css';
-import Carousel1 from './Carousel.jsx'
+import Carousel1 from './Carousel.jsx';
 
 export default class Home extends Component {
   render() {
